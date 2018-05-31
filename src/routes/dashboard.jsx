7ddbc833a2e94138f21c98d512cdde17bf95e7d5
Gutsover <mylenevandaele@hmail.com>
2018-05-31 11:52:31 +0100

    Initial commit

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,6 @@
+import Bills from 'views/Bills/Bills.jsx';
+
+var dashRoutes = [
+    { path: "/bills", name: "Bills", icon: "design_app", component: Bills }
+];
+export default dashRoutes;
---
+++ src/views/Bills/Bills.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Row, Col, Card } from "reactstrap";
+import { PanelHeader } from "components";
+
+const Bills = () => (
+  <div>
+    <PanelHeader size="sm" />
+    <div className="content">
+      <Row>
+        <Col xs={12}>
+          <Card>prout</Card>
+        </Col>
+      </Row>
+    </div>
+  </div>
+);
+
+export default Bills;
